22b2884e9bed43d8dbd34281995d0c23a892f2d7
wangyudi <wangyudi@bilibili.com>
2020-07-16 14:51:30 +0800

    no message

---
+++ Assets/Editor/Test.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Test
+{
+
+    [MenuItem("Tool/Build")]
+    public static void BuildTestMenuItem()
+    {
+        string path = Application.dataPath + "/../qqq.apk";
+    }
+
+    public static void BuildTest()
+    {
+        Debug.Log("========================================================");
+        Debug.Log("BuildTest");
+        Debug.Log("========================================================");
+
+        //string path = Application.dataPath + "/../qqq.apk";
+        Debug.Log("========================================================");
+        Debug.Log("======================================================== 1. ");
+        Debug.Log("======================================================== 2. ");
+    }
+}
